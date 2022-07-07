3b99da7e9aed9d8be15fb03414acc9ecaaaeb146
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2022-07-07 22:49:41 +0300

    cors packages added

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,25 @@
 import express, { Express, Request, Response } from 'express'
 import dotenv = require('dotenv')
+import cors = require('cors')
 dotenv.config()
 
 const app: Express = express()
+
+// parse json request body
+app.use(express.json());
+// parse urlencoded request body
+app.use(express.urlencoded({ extended: true }));
+// enable cors
+app.use(cors());
 
 
 app.get('/', (req: Request, res: Response) => {
     res.send(`<h1>This is starting of hel city bike app</h1>`)
 })
+
 const port = process.env.PORT
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`)
 })
+
+module.exports = app;
