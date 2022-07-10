e2661d27ad728de6c93a5ed1fe2c25cdc8c271b3
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2022-07-10 23:52:05 +0300

    add: init typeORM, demo to test routes:success

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,20 @@ import dotenv = require('dotenv')
 import cors = require('cors')
 dotenv.config()
 
-const app: Express = express()
+import { pgDataSource } from './app-data-source'
+import { User } from './db/entity/user.entity'
 
+// establish database connection
+pgDataSource
+    .initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!")
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization:", err)
+    })
+
+const app: Express = express()
 // parse json request body
 app.use(express.json());
 // parse urlencoded request body
@@ -14,12 +26,24 @@ app.use(cors());
 
 
 app.get('/', (req: Request, res: Response) => {
-    res.send(`<h1>This is starting of hel city bike app</h1>`)
+    res.send('hello world')
+})
+
+app.post("/users", async (req: Request, res: Response) => {
+    const user = await pgDataSource.getRepository(User).create(req.body)
+    const results = await pgDataSource.getRepository(User).save(user)
+    return res.send(results)
+})
+
+app.get("/list", async (req: Request, res: Response) => {
+    const user = await pgDataSource.getRepository(User).find()
+    return res.send(user)
 })
 
 const port = process.env.PORT
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`)
+
 })
 
 module.exports = app;
