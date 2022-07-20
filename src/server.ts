96e06a95377f1e2b6499dc644cd7185980ae8c94
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2022-07-20 14:41:02 +0300

    update: ready for a connection

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,12 @@
-import express, { Express, Request, Response } from 'express'
-import dotenv = require('dotenv')
+import express, { Express, NextFunction, Request, Response } from 'express'
 import cors = require('cors')
+const fs = require('fs');
+
+import { DBconnection } from './db';
+
+import dotenv = require('dotenv')
 dotenv.config()
 
-import { pgDataSource } from './config/db/app-data-source'
-import { User } from './db/entity/user.entity'
-
-// establish database connection
-pgDataSource
-    .initialize()
-    .then(() => {
-        console.log("Data Source has been initialized!")
-    })
-    .catch((err) => {
-        console.error("Error during Data Source initialization:", err)
-    })
 
 const app: Express = express()
 // parse json request body
@@ -24,24 +16,49 @@ app.use(express.urlencoded({ extended: true }));
 // enable cors
 app.use(cors());
 
+/* // this middleware could check for database connection
+app.use(async (req: Request, res: Response, next) => {
+    await DBconnection()
+
+}) */
+
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('hello world')
+    res.send('hello world welcome to world')
 })
 
-app.post("/users", async (req: Request, res: Response) => {
-    const user = await pgDataSource.getRepository(User).create(req.body)
-    const results = await pgDataSource.getRepository(User).save(user)
+/* app.post("/hsldata", async (req: Request, res: Response) => {
+    const data = await pgDataSource.getRepository(Hsl_city_bike_data_table).create(req.body)
+    const results = await pgDataSource.getRepository(Hsl_city_bike_data_table).save(data)
     return res.send(results)
-})
+}) */
 
-app.get("/list", async (req: Request, res: Response) => {
-    const user = await pgDataSource.getRepository(User).find()
-    return res.send(user)
-})
+/* app.get("/list", async (req: Request, res: Response) => {
+    try {
+        const user = await pgDataSource.getRepository(Hsl_city_bike_data_table)
+            .createQueryBuilder("hsl")
+            .where("hsl.returnstationname = :returnstationname", { returnstationname: "Merihaka" })
+            .getMany()
+
+        if (user.length === 0) {
+            throw new Error()
+        } else {
+            return res.json({ user })
+
+        }
+
+    } catch (error) {
+        res.json({ msg: error })
+
+
+    }
+
+}) */
 
 const port = process.env.PORT
-app.listen(port, () => {
+app.listen(port, async () => {
+    await DBconnection()
+
     console.log(`Server is running at http://localhost:${port}`)
 
 })
