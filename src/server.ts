09a2f11df55dfb9d9931a93edd7b9bfe50bb4afa
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2022-07-23 01:39:39 +0300

    refactor and added new routes

--- src/server.ts
+++ src/server.ts
@@ -1,66 +1,50 @@
-import express, { Express, NextFunction, Request, Response } from 'express'
-import cors = require('cors')
-const fs = require('fs');
+/**
+ *        @file server.ts
+ *     @summary Starting point of the application
+ * @description Handles the following middlwares:
+ *              - CORS
+ *              - API routes
+ *              - Auto Update Schema
+ *              - Server
+ */
 
-import { DBconnection } from './db';
+import express, { Express, NextFunction, Request, Response } from "express";
+import cors = require("cors");
+const fs = require("fs");
 
-import dotenv = require('dotenv')
-dotenv.config()
+import { DBconnection } from "./database";
 
+import dotenv = require("dotenv");
+import router from "./api/routers/journey";
+dotenv.config();
 
-const app: Express = express()
-// parse json request body
-app.use(express.json());
-// parse urlencoded request body
-app.use(express.urlencoded({ extended: true }));
-// enable cors
-app.use(cors());
+const app: Express = express();
 
-/* // this middleware could check for database connection
-app.use(async (req: Request, res: Response, next) => {
-    await DBconnection()
+async function main() {
+    await DBconnection();
+    // parse json request body
+    app.use(express.json());
+    // parse urlencoded request body
+    app.use(express.urlencoded({ extended: true }));
+    // enable cors
+    app.use(cors());
 
-}) */
+    //get data from route end-point
+    app.use("/api/hslcitybike", router);
 
+    app.get("/", (req: Request, res: Response) => {
+        res.send("hello world");
+    });
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('hello world welcome to world')
-})
+    const port = process.env.PORT || 5050;
 
-/* app.post("/hsldata", async (req: Request, res: Response) => {
-    const data = await pgDataSource.getRepository(Hsl_city_bike_data_table).create(req.body)
-    const results = await pgDataSource.getRepository(Hsl_city_bike_data_table).save(data)
-    return res.send(results)
-}) */
+    app.listen(port, () => {
+        console.log(
+            "\x1b[33m%s\x1b[0m",
+            `Server :: Running @ 'http://localhost:${port}'`
+        );
+    });
+}
+main();
 
-/* app.get("/list", async (req: Request, res: Response) => {
-    try {
-        const user = await pgDataSource.getRepository(Hsl_city_bike_data_table)
-            .createQueryBuilder("hsl")
-            .where("hsl.returnstationname = :returnstationname", { returnstationname: "Merihaka" })
-            .getMany()
-
-        if (user.length === 0) {
-            throw new Error()
-        } else {
-            return res.json({ user })
-
-        }
-
-    } catch (error) {
-        res.json({ msg: error })
-
-
-    }
-
-}) */
-
-const port = process.env.PORT
-app.listen(port, async () => {
-    await DBconnection()
-
-    console.log(`Server is running at http://localhost:${port}`)
-
-})
-
-module.exports = app;
+// module.exports = app;
