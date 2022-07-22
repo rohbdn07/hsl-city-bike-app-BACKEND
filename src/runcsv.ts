7f89c812126ee19c9dfdb43bbd099688cb136401
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2022-07-23 01:37:43 +0300

    update: formatted as rule and csv file update

--- src/runcsv.ts
+++ src/runcsv.ts
@@ -6,26 +6,32 @@ const fastcsv = require("fast-csv");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const absolutePath = path.join(__dirname, '/db/data/2021-07.csv')
+const absolutePath = path.join(__dirname, "/database/data/2021-07.csv");
 let stream = fs.createReadStream(absolutePath);
 // let stream = fs.createReadStream("./src/db/csvtest/test.csv");
 
-let csvData: any = [];
+let csvData: Array<any> = [];
 
-console.log('the relative path is', absolutePath)
+console.log("the relative path is", absolutePath);
 
 // at first, it parse CSV data into array and then inserts into database's table
-stream.pipe(fastcsv.parse())
-    .on('error', (error) => console.log(error))
-    .on('data', (row) => {
-        const result = Array.from(row, (v) => (v === undefined || v === '' ? null : v));
-        csvData.push(result)
+stream
+    .pipe(fastcsv.parse())
+    .on("error", (error: Error) =>
+        console.log(`Idle-Client Error:\n${error.message}\n${error.stack}`)
+    )
+    .on("data", (row: Array<any>) => {
+        const result = Array.from(row, (v) =>
+            v === undefined || v === "" ? null : v
+        );
+        csvData.push(result);
     })
-    .on('end', async (rowCount: number) => {
+    .on("end", async (rowCount: number) => {
         // remove the first line: header
         csvData.shift();
-        console.log('the length', csvData.length)
-        const query = 'INSERT INTO table2021_07 (departure_date, return_date, departure_station_id, departure_station_name, return_station_id, return_station_name, distance_meter, duration_sec) VALUES ($1, $2, $3, $4, $5, $6, $7, $8);'
+        console.log("the length", csvData.length);
+        const query =
+            "INSERT INTO table2021_07 (departure_date, return_date, departure_station_id, departure_station_name, return_station_id, return_station_name, distance_meter, duration_sec) VALUES ($1, $2, $3, $4, $5, $6, $7, $8);";
         //connection to database pool..
         const pool = new Pool({
             host: "localhost",
@@ -35,28 +41,25 @@ stream.pipe(fastcsv.parse())
             port: parseInt(`${process.env.POSTGRESQL_PORT}`) || 5432,
             max: 20,
             idleTimeoutMillis: 40000, // increase this value if connection is terminated without completing tasks.
-            allowExitOnIdle: false
+            allowExitOnIdle: false,
         });
 
-        pool.connect((err, client, done) => {
+        pool.connect((err: Error, client: any, done: any) => {
             if (err) throw err;
             try {
-                csvData.splice(0, 3000).forEach(row => {
+                csvData.splice(0, 3000).forEach((row) => {
                     client.query(query, row, (err, res) => {
                         if (err) {
                             console.log(err.stack);
+                            done();
                         } else {
                             console.log("inserted rows into database table...");
                         }
                     });
                 });
-                console.log(`Parsed ${rowCount} rows`)
+                console.log(`Parsed ${rowCount} rows`);
             } finally {
                 done();
             }
         });
-    }
-    )
-
-
-
+    });
