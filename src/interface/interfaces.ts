5a5374b4c6ed64d5671c06d32934d01f2b684d7a
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2022-07-23 02:00:05 +0300

    added: a seperate folder to define types and interfaces

---
+++ src/interface/interfaces.ts
@@ -0,0 +1,23 @@
+import { Response, Request } from "express";
+import { EntitySchema, Repository } from "typeorm";
+import { table_2021_05 } from "../database/entity/2021_05_hsl_entity";
+
+export interface IGetRowsResult {
+    success: boolean;
+    message: string;
+    data?: any;
+}
+
+export interface IExtendsRequest extends Request {
+    name: string;
+}
+
+export interface ITableName {
+    tableName: Repository<EntitySchema>;
+    departureStationName: EntitySchema<string>;
+}
+
+export type GetRowsOfTheMonthProps = {
+    month: string;
+    searchQuery: string;
+};
