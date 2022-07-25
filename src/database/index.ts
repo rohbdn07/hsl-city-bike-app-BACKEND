3b50f6c0f2cc6e4d706494a1d42e1f084c2c99c6
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2022-07-25 19:37:11 +0300

    Auto-sync ON for dev purpose

--- src/database/index.ts
+++ src/database/index.ts
@@ -4,33 +4,35 @@ dotenv.config();
 
 // connection to database(postgreSQL) while initializing
 export const DBconnection = async () => {
-    const client = await pgDataSource.initialize()
+    const client = await pgDataSource.initialize();
     try {
         if (client.isInitialized) {
             console.log(
-                '\x1b[33m%s\x1b[0m',
-                `Database is initialized and Connected...`,
-            )
-            const presentConnectionDBname = await client.createQueryRunner().getCurrentDatabase()
-            console.log('CONNECTION DATABASE NAME:', presentConnectionDBname)
-            // syncORnotSyncWithDatabase(client)
+                "\x1b[33m%s\x1b[0m",
+                `Database is initialized and Connected...`
+            );
+            const presentConnectionDBname = await client
+                .createQueryRunner()
+                .getCurrentDatabase();
+            console.log("CONNECTION DATABASE NAME:", presentConnectionDBname);
+            syncORnotSyncWithDatabase(client);
             return client;
         } else {
-            throw new Error("Unable to connect to database")
+            throw new Error("Unable to connect to database");
         }
     } catch (error) {
-        console.log('Unable to Connect!', error)
+        console.log("Unable to Connect!", error);
     }
-}
+};
 
 // at first check env varibale and do according to.
 // Sync MUST be turn FALSE when release into Production
 const syncORnotSyncWithDatabase = (client) => {
     if (Boolean(process.env.TYPEORM_SYNC_DEV)) {
-        client.synchronize()
-        console.log('Auto Synchronize is ON')
-        return
+        client.synchronize();
+        console.log("Auto Synchronize is ON");
+        return;
     } else {
-        console.log('Auto Synchronize is OFF')
+        console.log("Auto Synchronize is OFF");
     }
-}
+};
