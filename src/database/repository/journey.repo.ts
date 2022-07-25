36280f0fbe83bd3158d8c33434c328cf648d5e00
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2022-07-25 16:05:08 +0300

    refactor: minimize code with reusuable function

--- src/database/repository/journey.repo.ts
+++ src/database/repository/journey.repo.ts
@@ -2,93 +2,56 @@ import { GetRowsOfTheMonthProps } from "../../interface/interfaces";
 import { table_2021_05 } from "../entity/2021_05_hsl_entity";
 import { table_2021_06 } from "../entity/2021_06_hsl_entity";
 import { table_2021_07 } from "../entity/2021_07_hsl_entity";
+import { ILike } from "typeorm";
 
 /**
- * @file  journey.repo.ts
- * @param monthName name of the month by which we can get respective month's data collection
- * @returns collection of data including its total counts and name of month
+ * @file journey.repo.ts
+ * @param month name of the month
+ * @param searchQuery keyword to search
+ * @param pageSize skip values
+ * @param count no. of rows to display in a single page
+ * @returns collection of data(rows), total number of rows(count), month
  */
 export const getRowsOfTheMonth = async ({
     month,
     searchQuery,
+    pageSize,
+    count,
 }: GetRowsOfTheMonthProps) => {
-    console.log("REPO: this is for all journey rows", month);
+    console.log("REPO: this is for all journey rows", month, pageSize);
+    let hslData;
     try {
-        // Get all journey rows by search query name
-        async function getJourneyRowsByStationName(tableName, stationName) {
-            return await tableName
-                .createQueryBuilder("journey")
-                .where(
-                    "(journey.return_station_name Ilike :returnStationName OR journey.departure_station_name Ilike :departureStationName)"
-                )
-                .skip(0)
-                .take(20)
-                .setParameters({
-                    returnStationName: `%${stationName}%`,
-                    departureStationName: `%${stationName}%`,
-                })
-                .getMany();
-        }
-
-        function returnDataWithCountAndMonth(data, month) {
-            let hslData;
-            let count;
-            const monthName = month;
-            if (searchQuery) {
-                return {
-                    hslData: data,
-                    monthName,
-                    count: data?.length,
-                };
-            }
-            const totalCount = data ? data.pop() : undefined;
-            hslData = data.shift();
-            count = totalCount;
-            return {
-                hslData,
-                count,
-                monthName,
-            };
-        }
-
         switch (month) {
             case "may":
-                const hslData1 = !searchQuery
-                    ? await table_2021_05.findAndCount({
-                          take: 20,
-                          skip: 0,
-                      })
-                    : await getJourneyRowsByStationName(
-                          table_2021_05,
-                          searchQuery
-                      );
-                const data1 = returnDataWithCountAndMonth(hslData1, month);
-
-                return data1;
-
+                hslData = await search(
+                    table_2021_05, // name of table
+                    month,
+                    searchQuery,
+                    pageSize,
+                    count
+                );
+                return hslData;
                 break;
             case "june":
-                const hslData2 = !searchQuery
-                    ? await table_2021_06.findAndCount()
-                    : await getJourneyRowsByStationName(
-                          table_2021_06,
-                          searchQuery
-                      );
-                const data2 = returnDataWithCountAndMonth(hslData2, month);
-                return data2;
-
+                hslData = await search(
+                    table_2021_06, // name of table
+                    month,
+                    searchQuery,
+                    pageSize,
+                    count
+                );
+                return hslData;
                 break;
             case "july":
-                const hslData3 = !searchQuery
-                    ? await table_2021_07.findAndCount()
-                    : await getJourneyRowsByStationName(
-                          table_2021_07,
-                          searchQuery
-                      );
-                const data3 = returnDataWithCountAndMonth(hslData3, month);
-                return data3;
+                hslData = await search(
+                    table_2021_07, // name of table
+                    month,
+                    searchQuery,
+                    pageSize,
+                    count
+                );
+                return hslData;
                 break;
-
             default:
                 throw new Error();
                 break;
@@ -96,4 +59,39 @@ export const getRowsOfTheMonth = async ({
     } catch (error) {
         console.log("the error", error);
     }
+};
+
+const search = async (
+    tableName,
+    month,
+    searchQuery,
+    pageSize,
+    numberOfRowsToDisplay
+) => {
+    // find data(rows) from postgreSQL database tables
+    const [list, count] = await tableName.findAndCount({
+        select: {
+            id: true,
+            departure_station_name: true,
+            return_station_name: true,
+            distance_meter: true,
+            duration_sec: true,
+        },
+        where: searchQuery
+            ? [
+                  { departure_station_name: ILike(`%${searchQuery}%`) },
+                  {
+                      return_station_name: ILike(`${searchQuery}`),
+                  },
+              ]
+            : {},
+        skip: pageSize,
+        take: numberOfRowsToDisplay,
+    });
+
+    return {
+        list,
+        count,
+        month,
+    };
 };
