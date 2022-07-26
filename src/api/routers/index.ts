ee33a5452da8acb898dec6836d80aa096ece10d4
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2022-07-26 15:26:07 +0300

    update: added a new route for station list

--- src/api/routers/index.ts
+++ src/api/routers/index.ts
@@ -1,10 +1,11 @@
-import { Router } from 'express'
+import { Router } from "express";
 
-import journeyList from './journey'
+import journeyList from "./journey/index";
+import stationsList from "./stations/index";
 
+const router = Router();
 
-const router = Router()
+router.use(journeyList);
+router.use(stationsList);
 
-router.use(journeyList)
-
-export default router
+export default router;
