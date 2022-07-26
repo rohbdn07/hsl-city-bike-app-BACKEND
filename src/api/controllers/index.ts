f476cb017d3bb73f562ba3a006aed7dfd1b97c9a
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2022-07-26 15:25:01 +0300

    added: a new route to show all stations

--- src/api/controllers/index.ts
+++ src/api/controllers/index.ts
@@ -1,5 +1,7 @@
-import { showJourneyList } from './journey.controller'
+import { showJourneyList } from "./journey.controller";
+import { showStationList } from "./station.controller";
 
 export default {
     showJourneyList,
-}
+    showStationList,
+};
