0c536a87cee75cb86eabc97eac42bcbcdfeed047
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2022-07-28 12:13:24 +0300

    added: middleware route

--- src/api/middlewares/index.ts
+++ src/api/middlewares/index.ts
@@ -1,6 +1,7 @@
-import { checkSearchQuery } from "./check.queries";
+import { checkSearchQuery, checkQueryForStationList } from "./check.queries";
 import { checkMonthValidation } from "./check.month";
 export default {
     checkSearchQuery,
+    checkQueryForStationList,
     checkMonthValidation,
 };
