f1adc0a77ccb413d4a2a8a7c66c8560f829d8532
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2022-07-28 12:16:24 +0300

    added where condition to get rows by name or stationid

--- src/database/repository/station.repo.ts
+++ src/database/repository/station.repo.ts
@@ -1,11 +1,13 @@
+import { ILike } from "typeorm";
+import { IStationQuery } from "../../interface/interfaces";
 import { bike_station } from "../entity/hsl_bike_station.entity";
 
 /**
  * @file station.repo.ts
  * @returns collection of data(rows), total number of rows(count)
  */
-export const getRowsOfStation = async () => {
-    console.log("station repo is called");
+export const getRowsOfStation = async (props: IStationQuery) => {
+    const numberOfRowsToDisplay = props.count;
     try {
         const [list, count] = await bike_station.findAndCount({
             select: {
@@ -17,6 +19,15 @@ export const getRowsOfStation = async () => {
                 operaattor: true,
                 kapasiteet: true,
             },
+            where:
+                props.name || props.stationid
+                    ? [
+                          { name: ILike(`%${props.name}`) },
+                          { station_id: props.stationid },
+                      ]
+                    : {},
+            skip: props.pageSize,
+            take: numberOfRowsToDisplay,
         });
         return {
             data: list,
