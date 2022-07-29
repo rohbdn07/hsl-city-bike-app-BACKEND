923c01e4db72e5310b9f3625aab6970f6e59947d
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2022-07-29 16:50:13 +0300

    added another middleware

--- src/api/middlewares/index.ts
+++ src/api/middlewares/index.ts
@@ -1,7 +1,12 @@
-import { checkSearchQuery, checkQueryForStationList } from "./check.queries";
+import {
+    checkSearchQuery,
+    checkQueryForStationList,
+    checkStationIdParams,
+} from "./check.queries";
 import { checkMonthValidation } from "./check.month";
 export default {
     checkSearchQuery,
     checkQueryForStationList,
     checkMonthValidation,
+    checkStationIdParams,
 };
