fe95683bb6205ece645aff78b5832aca6cd69955
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2022-07-29 16:56:14 +0300

    added: a new service and put logic to get station info by id and specific month

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,8 @@
 import { getRows } from "./journey.service";
-import { getStationRows } from "./station.service";
+import { getStationRows, getInfoById } from "./station.service";
 
 export default {
     getRows,
     getStationRows,
+    getInfoById,
 };
